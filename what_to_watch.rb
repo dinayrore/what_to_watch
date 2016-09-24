8c46d35c3270c623e466df540907b3fd637a2a22
Kristine <kteich88@gmail.com>
2016-09-24 13:56:53 -0400

    Added methods to main.

--- what_to_watch.rb
+++ what_to_watch.rb
@@ -1,6 +1,6 @@
 require '../movie_lens'
 
-def start_up
+def start_program
   puts 'Are you bored? Unsure of what to do with all your free time?'
   puts "Why not watch a movie? Don't worry, we'll make the decision for you!"
   puts 'How would you like to conduct your search?'
@@ -10,31 +10,43 @@ def prompt_user_search
   puts 'Please choose from the following options'
   puts '(1) Search by movie ID number.'
   puts '(2) Search by user ID'
-  number = gets.chomp
-  if !%w(1 2).include?(number)
+  @user_input = gets.chomp
+  if !%w(1 2).include?(@user_input)
     puts 'Invalid input. Try again.'
     prompt_user_search
-  else
-    if number == 1
-      number = @movie_id
-    else
-      number = @user_id
   end
 end
 
-def search_for
+def search_by_movie_id
+  if @user_input == 1
+    puts 'Please choose from the following options'
+    puts '(1) Find all ratings'
+    puts '(2) Find average ratings'
+    puts '(3) Find movie title'
+    @user_input = gets.chomp
+    if !%w(1..3).include?(@user_input)
+      puts 'Would you like to go back to the main menu options? Type \'Y\'.'
+      @user_input = gets.chomp.upcase
+        if @user_input == 'Y'
+          prompt_user_search
+        else
+          puts 'Invalid input. Try again.'
+          search_by_movie_id
+    elsif
 
-  puts 'Please choose from the following options'
-  Find all ratings for a movie by id
-Find the average rating for a movie by id
-Find the name of a movie by id
-Find all ratings for a user
+    else
+
+
+  end
+  else
+    puts 'Would you like to search all ratings made by a user?'
+
 end
 
 def main
-  start_up
+  start_program
   prompt_user_search
-  search_for
+  search_by_movie_id
 end
 
 main if __FILE__ == $PROGRAM_NAME
